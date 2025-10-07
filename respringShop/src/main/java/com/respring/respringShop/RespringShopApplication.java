472b23918fc4b869721f6ac39b731724cbbba666
HoldRain47 <holdrain47@gmail.com>
2025-10-07 18:05:15 +0900

    Add BE directories

---
+++ respringShop/src/main/java/com/respring/respringShop/RespringShopApplication.java
@@ -0,0 +1,13 @@
+package com.respring.respringShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RespringShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RespringShopApplication.class, args);
+	}
+
+}
---
+++ respringShop/src/test/java/com/respring/respringShop/RespringShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.respring.respringShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RespringShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
